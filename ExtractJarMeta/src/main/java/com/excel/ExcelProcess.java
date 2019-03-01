4868beed36dfdbdce1231a2a66a5ff5b480b025e
Kranthi Kumar Jakkula <kjakkula@progress.com>
2019-03-01 18:30:58 +0530

    Added for Excel Testing

---
+++ ExtractJarMeta/src/main/java/com/excel/ExcelProcess.java
@@ -0,0 +1,22 @@
+package com.excel;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class ExcelProcess {
+
+	public static void main(String[] args) {
+		Map<String, String> map=new HashMap<String, String>();
+		Set<String> set= new HashSet<String>();
+		
+		//TODO    Set some feilds as Headings,   map   Some Extra feilds
+		
+		
+		
+		
+		
+	}
+
+}
